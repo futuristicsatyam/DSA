8954697ded3979eef608121a15dbc6dc3766589f
Satyam Sharma <106440183+futuristicsatyam@users.noreply.github.com>
2026-03-15 23:35:35 +0530

    Update content.module.ts

--- apps/api/src/content/content.module.ts
+++ apps/api/src/content/content.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { CacheModule } from "../cache/cache.module";
+import { DatabaseModule } from "../database/database.module";
 import { ContentController } from "./content.controller";
 import { ContentService } from "./content.service";
 
 @Module({
+  imports: [DatabaseModule, CacheModule],
   controllers: [ContentController],
   providers: [ContentService],
   exports: [ContentService]
