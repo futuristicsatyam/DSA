843dc53fa5c1605535aac66a38a1579c633f8605
Satyam Sharma <106440183+futuristicsatyam@users.noreply.github.com>
2026-03-15 23:37:08 +0530

    Update bookmarks.module.ts

--- apps/api/src/bookmarks/bookmarks.module.ts
+++ apps/api/src/bookmarks/bookmarks.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
+import { DatabaseModule } from "../database/database.module";
 import { BookmarksController } from "./bookmarks.controller";
 import { BookmarksService } from "./bookmarks.service";
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [BookmarksController],
   providers: [BookmarksService]
 })
