ec059aee75f8460312867bb95a6c6ff99cf87536
Satyam Sharma <106440183+futuristicsatyam@users.noreply.github.com>
2026-03-16 16:48:27 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,20 @@
+import { defineConfig, globalIgnores } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
-export default [
+export default defineConfig([
   {
     ignores: ["**/dist/**", "**/.next/**", "**/coverage/**", "**/node_modules/**"]
   },
+
   js.configs.recommended,
   ...tseslint.configs.recommended,
+  ...nextVitals,
+  ...nextTs,
+
   {
     files: ["apps/api/**/*.ts", "apps/web/**/*.ts", "apps/web/**/*.tsx", "packages/**/*.ts", "packages/**/*.tsx"],
     languageOptions: {
@@ -20,8 +27,20 @@ export default [
         ...globals.browser
       }
     },
+    settings: {
+      next: {
+        rootDir: ["apps/web/"]
+      }
+    },
     rules: {
       "@typescript-eslint/no-explicit-any": "off"
     }
-  }
-];
+  },
+
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts"
+  ])
+]);
